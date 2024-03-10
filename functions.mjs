e4e3886068c95351acfd9be3643287560fa2af6c
Luigi Carvalho <carvalholuigi25@gmail.com>
2024-03-10 19:02:53 +0000

    removed confirm prompt

--- functions.mjs
+++ functions.mjs
@@ -7,24 +7,22 @@ import fs from 'fs';
 import ProgressBar from 'progress';
 import { fileURLToPath } from 'url';
 
-// prompts.override(require('yargs').argv);
-
 export function getAryLinks() {
   return [
-    { 
-      title: 'KUbuntu 22.04.4 (x64)', 
-      value: 'https://cdimage.ubuntu.com/kubuntu/releases/22.04.4/release/kubuntu-22.04.4-desktop-amd64.iso', 
-      name: 'kubuntu-22.04.4-desktop-amd64.iso' 
+    {
+      title: 'KUbuntu 22.04.4 (x64)',
+      value: 'https://cdimage.ubuntu.com/kubuntu/releases/22.04.4/release/kubuntu-22.04.4-desktop-amd64.iso',
+      name: 'kubuntu-22.04.4-desktop-amd64.iso'
     },
-    { 
-      title: 'Linux Mint (Cinnamon) (x64)', 
+    {
+      title: 'Linux Mint (Cinnamon) (x64)',
       value: 'https://mirrors.cicku.me/linuxmint/iso/stable/21.3/linuxmint-21.3-cinnamon-64bit.iso',
-      name: 'linuxmint-21.3-cinnamon-64bit.iso' 
+      name: 'linuxmint-21.3-cinnamon-64bit.iso'
     },
-    { 
-      title: 'Manjaro KDE 23.1.3 (x64)', 
+    {
+      title: 'Manjaro KDE 23.1.3 (x64)',
       value: 'https://download.manjaro.org/kde/23.1.3/manjaro-kde-23.1.3-240113-linux66.iso',
-      name: 'manjaro-kde-23.1.3-240113-linux66.iso' 
+      name: 'manjaro-kde-23.1.3-240113-linux66.iso'
     }
   ];
 }
@@ -60,31 +58,22 @@ export async function doDownload(fileUrl, fileName, title) {
 
 
 export async function doAsk() {
-  (async () => {
-    const onSubmit = async (prompt, answer) => {
+  const onSubmit = async (prompt, answer) => {
+    if (answer.length > 0) {
       await doDownload(prompt.choices[0].value, prompt.choices[0].name, prompt.choices[0].title);
-    };
-    
-    const onCancel = prompt => {
-      console.log('Bye!');
-      return true;
-    };
+    } else {
+      console.log('You should select at least one or more softwares to download!');
+    }
+  };
 
-    const questions = [
-      {
-        type: 'multiselect',
-        name: 'text',
-        message: 'Select the OS',
-        choices: getAryLinks(),
-      },
-      {
-        type: 'confirm',
-        name: 'value',
-        message: 'Can you confirm?',
-        initial: true
-      }
-    ];
+  const questions = [
+    {
+      type: 'multiselect',
+      name: 'softwaresel',
+      message: 'Select the OS',
+      choices: getAryLinks()
+    }
+  ];
 
-    await prompts(questions, { onSubmit, onCancel });
-  })();
+  await prompts(questions, { onSubmit });
 }
